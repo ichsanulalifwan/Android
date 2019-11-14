17ed63a65307b8ae1a2b6413890804cb425deeb7
Ichsanul Alifwan <55717141+ichsanulalifwan@users.noreply.github.com>
2019-11-14 14:12:58 +0800

    Create Athlete.java

---
+++ Athlete.java
@@ -0,0 +1,73 @@
+package com.dicoding.picodiploma.submission_fastestman.model;
+
+
+public class Athlete {
+    private String name;
+    private String category;
+    private int photo;
+    private String country;
+    private String bio;
+    private int foto;
+    private int flag;
+
+
+    public Athlete() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getCoutry() {
+        return country;
+    }
+
+    public void setCoutry(String coutry) {
+        this.country = coutry;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+}
